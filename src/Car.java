6489867762d19e9e5e1dfe64b1fee82ea5a24474
amann <amann@10.64.145.224>
2019-12-10 10:23:54 -0600

    First commit.

---
+++ src/Car.java
@@ -0,0 +1,53 @@
+/********************************************************************
+ * Programmer:	sveinson
+ * Class:  CS30S
+ *
+ * Assignment: AxQy
+ *
+ * Description: Class file for the car class
+ *	each car object has a fuel efficiency measured in litres per 100 km
+ *	each car object has a fuel property, how much gas is in the tank 
+ *	measured in litres.
+ *	You are to determine what methods are needed and implement  them
+ *	in an efficient manner.
+**************************************************************************/
+
+
+// import libraries as needed here
+
+public class Car {
+    //*** Class Variables ***
+    
+    //*** Instance Variables ***
+    
+    //*** Constructors ***
+    
+    /*****************************************
+    * Description: default constructor
+    * 
+    * Interface:
+    * 
+    * @param        each parameter of the method should be listed with an @param
+    * @param        parametername description of parameter
+    * 
+    * @return       any return value will be noted here
+    * ****************************************/
+        public Car(){
+            
+        }
+    /*****************************************
+    * Description: intialized constructor
+    * 
+    * Interface:
+    * 
+    * @param        each parameter of the method should be listed with an @param
+    * @param        parametername description of parameter
+    * 
+    * @return       any return value will be noted here
+    * ****************************************/
+    
+    //*** Getters ***
+    
+    //*** Setters ***
+    
+} // end of public class
